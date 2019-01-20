f3090737c71088c4540dc7800d0211fbb9d1bb9e
Kadar Mohamed <kadarmohamed@gmail.com>
2019-01-20 15:52:03 +0100

    encoding user input

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const request = require('request')
-const config = require('./config')
+const yargs = require('yargs')
 
+const config = require('./config')
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv.a)
+const address = encodeURIComponent(argv.a)
 
 request({
-  url: config.configAddress.url,
+  url: `${config.configAddress.url}${address}`,
   json: true
 }, (error, response, body) => {
   /* console.log(JSON.stringify(response, undefined, 2)) */
