5d32bd1cd68d93a15242e21ab02b2d8c853ac6c6
Kadar Mohamed <kadarmohamed@gmail.com>
2019-01-20 13:05:54 +0100

    call stack & event loop

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -5,7 +5,12 @@ setTimeout(() => {
 }, 2000)
 
 setTimeout(() => {
-  console.log('Second setTimeout')
+  console.log('2nd setTimeout')
 }, 0)
 
+console.log('4...')
+setTimeout(() => {
+  console.log('3rd setTimeout')
+}, 0)
+console.log('5...')
 console.log('Finishing up...')
