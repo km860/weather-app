505ddef8333c8ad6c39012408b8939764a74ba8a
Kadar Mohamed <kadarmohamed@gmail.com>
2019-01-20 15:32:43 +0100

    what makes up an HTTP request

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ request({
   url: config.configAddress.url,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2))
+  /* console.log(JSON.stringify(response, undefined, 2)) */
+  console.log(`Address: ${body.results[0].formatted_address}`)
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 })
