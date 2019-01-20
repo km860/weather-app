1d830f8bf5ba2e81a314415b73c35939d5f64a20
Kadar Mohamed <kadarmohamed@gmail.com>
2019-01-20 18:33:12 +0100

    wiring up weather search

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yargs = require('yargs')
+/* const yargs = require('yargs')
 const geocode = require('./geocode/geocode')
 const argv = yargs
   .options({
@@ -19,4 +19,19 @@ geocode.geocodeAddress(argv.a, (errorMessage, results) => {
   } else {
     console.log(JSON.stringify(results, undefined, 2))
   }
+}) */
+const request = require('request')
+
+request({
+  url: ' https://api.darksky.net/forecast/2b77614432ca4f8a9d9cacb7d0389228/40.7134104,-74.0069091',
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature)
+  } else {
+    console.log('Unable to fetch weather')
+  }
 })
+
+// 2b77614432ca4f8a9d9cacb7d0389228
+// https://api.darksky.net/forecast/2b77614432ca4f8a9d9cacb7d0389228/40.7134104,-74.0069091
